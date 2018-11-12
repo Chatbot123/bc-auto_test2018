c46cd63c50dc314b6865f114ae7ac7d22535b1ba
Chatbot123 <42810864+Chatbot123@users.noreply.github.com>
2018-11-12 13:35:34 +0530

    Update index.php

--- index.php
+++ index.php
@@ -130,6 +130,62 @@ if($method == 'POST')
 		
 
 	}
+	if($json->queryResult->intent->displayName=='Raise_ticket_intent - GetnameGetissue - yes - yes')
+	{
+		//if(isset($json->queryResult->queryText))
+		//{ $sh_desc = $json->queryResult->queryText; }
+
+		if(isset($json->queryResult->parameters->name))
+		{ $name = $json->queryResult->parameters->name; }
+		
+		if(isset($json->queryResult->parameters->issue))
+		{ $sh_desc = $json->queryResult->parameters->issue; }
+
+		$sh_desc = strtolower($sh_desc);
+		//$sh_desc = "Testing";
+		//$name = "someone";
+		$instance = "dev60887";
+		$username = "admin";
+		$password = "Avik.17.jan";
+		$table = "incident";
+		//$jsonobj = "{\"short_description\":$sh_desc,\"priority\":\"1\",\"Caller_id\":\"someone\"}";
+		$jsonobj = array('short_description' => $sh_desc);
+             	$jsonobj = json_encode($jsonobj);	
+
+		//$jsonobj = "{\"short_description\":$sh_desc,\"priority\":\"1\",\"Caller_id\":$name}";
+		$query = "https://$instance.service-now.com/$table.do?JSONv2&sysparm_action=insert";
+		$curl = curl_init($query);
+		curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+		curl_setopt($curl, CURLOPT_USERPWD, "$username:$password");
+		curl_setopt($curl, CURLOPT_VERBOSE, 1);
+		curl_setopt($curl, CURLOPT_HEADER, false);
+		curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+		curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+		if($jsonobj)
+		{
+			    curl_setopt($curl, CURLOPT_POST, true);
+			    curl_setopt($curl, CURLOPT_HTTPHEADER, array("Content-Type: application/json"));
+			    curl_setopt($curl, CURLOPT_POSTFIELDS, $jsonobj);
+		}
+		$response = curl_exec($curl);
+		curl_close($curl);
+		$jsonoutput = json_decode($response);
+		$incident_no =  $jsonoutput->records[0]->number;
+		$sys_id = $jsonoutput->records[0]->sys_id;
+		$json->queryResult->parameters->incident_num= $incident_no;
+		$json->queryResult->parameters->sys_id= $sys_id;
+		echo $json->queryResult->parameters->incident_num;
+		echo $json->queryResult->parameters->sys_id;
+		$speech = "Thanks ".$name."! Incident Created Successfully for issue " . $sh_desc . " and your incident number is " . $incident_no;
+		$speech .= " Sys_id is ".$sys_id;
+		$speech .= "\r\n";
+		$speech .= " Thanks for contacting us. Are you satisfied with the response?";
+		//echo $speech;
+		
+
+	}
 	
 	//--------------------
 	if($json->queryResult->intent->displayName=='SENDMAIL')
